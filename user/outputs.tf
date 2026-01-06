dc63681e9bc1c9bdb40e3c9be4971ca0b5df7041
Nishwanth-eq <nishwanth@equicomtech.com>
2026-01-06 14:50:41 +0530

    Add AWS account ID and login instructions outputs
    Added outputs for AWS account ID and login instructions.

--- user/outputs.tf
+++ user/outputs.tf
@@ -16,3 +16,22 @@ output "console_passwords" {
   }
   sensitive = true
 }
+
+output "aws_account_id" {
+  description = "AWS Account ID for console login"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "login_instructions" {
+  description = "Console login information and credentials for all users"
+  value = {
+    for k, u in aws_iam_user.users :
+    k => {
+      username       = u.name
+      login_url      = "https://${data.aws_caller_identity.current.account_id}.signin.aws.amazon.com/console"
+      account_id     = data.aws_caller_identity.current.account_id
+      password_cmd   = "terraform output -json console_passwords | jq -r '.${k}'"
+      instructions   = "1. Visit the login_url above\n2. Enter Account ID: ${data.aws_caller_identity.current.account_id}\n3. Enter IAM username: ${u.name}\n4. Get password from: terraform output -json console_passwords | jq -r '.${k}'\n5. You will be forced to change password on first login"
+    }
+  }
+}
