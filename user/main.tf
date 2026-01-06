b2791b9d0095e034e3c12125cfcdc1da0e08a9a2
Nishwanth-eq <nishwanth@equicomtech.com>
2026-01-06 14:01:55 +0530

    Implement password generation and login profile setup
    Added password generation and login profile creation for IAM users.

--- user/main.tf
+++ user/main.tf
@@ -2,6 +2,12 @@ resource "aws_iam_user" "users" {
   for_each = toset(var.usernames)
   name     = each.value
   path     = "/"
+  force_destroy  = true
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "aws iam delete-login-profile --user-name ${self.name} --region us-east-1 || true"
+  }
 }
 
 # Add each created user into the provided devops group
@@ -28,4 +34,33 @@ resource "aws_iam_user_policy" "switch_role" {
       }
     ]
   })
+}
+
+resource "random_password" "user_password" {
+  for_each = aws_iam_user.users
+
+  length      = 16
+  special     = true
+  min_upper   = 2
+  min_lower   = 2
+  min_numeric = 2
+  min_special = 2
+}
+
+#resource "aws_iam_user_login_profile" "this" {
+#  for_each = aws_iam_user.users
+#
+#  user                    = each.value.name
+#  password                = random_password.user_password[each.key].result
+#  password_reset_required = true
+#}
+
+resource "null_resource" "create_login_profile" {
+  for_each = aws_iam_user.users
+
+  provisioner "local-exec" {
+    command = "aws iam create-login-profile --user-name ${each.value.name} --password ${jsonencode(random_password.user_password[each.key].result)} --password-reset-required --region us-east-1"
+  }
+
+  depends_on = [aws_iam_user.users]
 }
