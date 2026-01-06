2110206936f32bc9af3a5eb627a15982e49398a0
Nishwanth-eq <nishwanth@equicomtech.com>
2026-01-06 14:51:42 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,13 @@ output "console_passwords" {
   value       = module.user.console_passwords
   sensitive   = true
 }
+
+output "login_instructions" {
+  description = "Console login information and credentials for all users"
+  value       = module.user.login_instructions
+}
+
+output "get_passwords_command" {
+  description = "Command to retrieve sensitive passwords"
+  value       = "terraform output -json console_passwords | jq -r '.'"
+}
